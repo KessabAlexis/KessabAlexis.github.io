12b54c05370978bb36f5e89224ecf068b7c6129e
Alexis Kessab <alexis.kessab@hetic.net>
2020-02-04 16:21:50 +0100

    ajout js scroll reveal sur 2eme slogan

--- scripts/app.js
+++ scripts/app.js
@@ -14,3 +14,15 @@ sr.reveal('.reveal_left_brosse1',{
   opacity:0,
   reset:true,
 });
+
+sr.reveal('.scroll_sl0',{
+  duration:3000,
+  reset:true,
+  opacity:0,
+});
+
+sr.reveal('.scroll_sl1',{
+  reset:true,
+  opacity:0,
+  duration:1000,
+});
