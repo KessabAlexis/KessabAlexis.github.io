93cd9a1ba538241bb3e49cfb2e3a82b9a1026e66
Alexis Kessab <alexis.kessab@hetic.net>
2020-02-05 10:11:34 +0100

    aa

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,15 @@ const sr = ScrollReveal();
 
 sr.reveal('.reveal_right_oralb',{
   origin:'right',
-  duration:3400,
-  distance:'700px',
+  duration:1800,
+  distance:'1650px',
   opacity:0,
   reset:true,
 });
 
 sr.reveal('.reveal_left_brosse1',{
   origin:'left',
-  duration:2000,
+  duration:1200,
   distance:'600px',
   opacity:0,
   reset:true,
@@ -28,10 +28,34 @@ sr.reveal('.scroll_sl1',{
   duration:1000,
 });
 
-sr.reveal('.scroll_normaltext',{
+sr.reveal('.scroll_normaltext_left',{
   reset:true,
   opacity:0,
   duration:4000,
   distance:'1200px',
   origin:'left',
+});
+
+sr.reveal('.scroll_normaltext_right',{
+  reset:true,
+  opacity:0,
+  duration:4000,
+  distance:'1200px',
+  origin:'right',
+});
+
+sr.reveal('.scroll_chargeur',{
+  reset:true,
+  opacity:0,
+  duration:1500,
+  distance:'800px',
+  origin:'left',
+});
+
+sr.reveal('.scroll_brosse_charg',{
+  reset:true,
+  opacity:0,
+  duration:2000,
+  distance:'300px',
+  origin:'right',
 });
